2aa333caa684f03caa71127849660c0fe530ba42
Business Fawcett <fawcettbusiness@gmail.com>
2026-05-26 15:55:56 -0600

    Fix: Add props typing to ShoppingCartIcon component to accept className prop

--- components/ShoppingCartIcon.tsx
+++ components/ShoppingCartIcon.tsx
@@ -1,6 +1,6 @@
-export default function ShoppingCartIcon() {
+export default function ShoppingCartIcon({ className = "" }: { className?: string }) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-6 h-6 ${className}`}>
       <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.07 60.07 0 01-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 01-.75.75H4.5a.75.75 0 01-.75-.75V19.5h4.125M18 20.25a.75.75 0 01-.75.75H16.5a.75.75 0 01-.75-.75V19.5h4.125z" />
     </svg>
   );
