d0a5d627a8da42cf905a7894313876ee34c05db5
Business Fawcett <fawcettbusiness@gmail.com>
2026-05-26 17:19:05 -0600

    Fix groq import - use groq package directly

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { client } from "@/sanity/lib/client";
-import { groq } from "next-sanity/groq";
+import { groq } from "groq";
 import Image from "next/image";
 
 // GROQ queries to fetch data
